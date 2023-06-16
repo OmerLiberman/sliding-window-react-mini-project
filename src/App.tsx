9b6cab4cf2e166c40fca5fe082b4cd417d22e9a7
Omer Liberman <omer@Omers-MacBook-Pro.local>
2023-06-16 14:53:00 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const fetchPropsals = async (text?: string) => {
+  console.log('text', text);
+  return [
+    'propsal 1',
+    'propsal 2',
+    'propsal 3',
+  ]
+}
+
 function App() {
+  const [propsals, setPropsals] = React.useState<Array<string>>([]);
+  const [position, setPosition] = React.useState<{top?: number, left?: number}>({});
+
+  React.useEffect(() => {
+    document.addEventListener("selectionchange", () => {
+      const selection = document.getSelection();
+      const selectedText = selection?.toString();
+
+      const lowerRange = selection?.focusOffset;
+      const higherRange = selection?.anchorOffset;
+      const existingText = selection?.anchorNode?.textContent;
+
+      const rect = selection?.getRangeAt(0).getBoundingClientRect();
+      setPosition({top: rect?.top, left: rect?.left! + rect?.width!})
+      fetchPropsals(selectedText).then(res => setPropsals(res))
+    });
+
+    document.addEventListener('pointerdown', (event) => {
+      document.getSelection()?.removeAllRanges();
+      event.stopPropagation();
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div contentEditable className='editor'>
+        Some editable content here ff
+      </div>
+      {position && <div style={{background: 'blue', position: 'absolute', ...position}}>
+          {propsals.map(propsal => <div> {propsal} </div>)}
+      </div>}
     </div>
   );
 }
